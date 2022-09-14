183f6c95c28cc70f753d332751ad719295984f65
Kadir Avşar <54157732+Kadiiravsaar@users.noreply.github.com>
2022-09-14 11:39:54 +0300

    Add project files.

---
+++ NLayer.Core/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+
+        // bire çok ilişki olacağı için
+        public int CategoryId { get; set; }// burada bu foregin key olur
+        public Category Category { get; set; }
+
+        public ProductFeature ProductFeature { get; set; }
+    }
+}
